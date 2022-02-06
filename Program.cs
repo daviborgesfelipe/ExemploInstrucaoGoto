4b4cb0da60807b54788b4534b0c62102ac62c46e
Davi Borges <davifelipe_300@hotmail.com>
2022-02-06 12:04:09 -0300

    Instrução goto, como se comporta

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ExemploInstrucaoGoto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            string nome;
+            string continuar;
+
+            Inicio:
+            Console.WriteLine("\n#############################");
+            Console.Write("Qual seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine($"Olá {nome}");
+            
+            Console.WriteLine("Deseja continuar: Sim[s]  Não[n]");
+            continuar = Console.ReadLine();
+
+            if(continuar == "s")
+            {
+                goto Inicio;
+            }
+            
+        }
+    }
+}
